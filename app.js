d6e2f4fd734a57fb893fe7adaf88b0b1f9e596b9
Unknown <zdenek.pribyla@gmail.com>
2018-10-30 10:01:33 +0100

    add window.open for all links in array

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ $(document).ready(function () {
 
 	$("#my-button").click(function () {
 
-		$.get('data.json', function (data) {
-			data.forEach(function (singleLink) {
-				var singleIdLink = singleLink.idLink
-				var getClassName = $('#my-button').attr('class');
-				if (getClassName == singleIdLink) {
-					alert("bingo, je to " + getClassName)
+		$.get('data.json', function (items) {
+			items.forEach(function (item) {
+				var singleLinkId = item.linkId
+				var classNameOfClickElement = $('#my-button').attr('class');
+				if (classNameOfClickElement === singleLinkId) {
+					item.linkUrl.forEach(function (singleLinkUrl) {
+						window.open(singleLinkUrl);
+					});
 				};
 			});
 
