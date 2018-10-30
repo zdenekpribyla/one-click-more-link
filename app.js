e5c472f2bf0e483a1654b8b803a422c6b70b63e2
Unknown <zdenek.pribyla@gmail.com>
2018-10-30 21:57:18 +0100

    fixed problem with Google Chrome blocks a new window.open

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 $(document).ready(function () {
-
-	$("#my-button").click(function () {
-
-		$.get('data.json', function (items) {
-			items.forEach(function (item) {
-				var singleLinkId = item.linkId
-				var classNameOfClickElement = $('#my-button').attr('class');
-				if (classNameOfClickElement === singleLinkId) {
-					item.linkUrl.forEach(function (singleLinkUrl) {
+	$.get('data.json', function (items) {
+		items.forEach(function (item) {
+			var singleLinkId = item.linkId
+			var classNameOfClickElement = $('#my-button').attr('class');
+			if (classNameOfClickElement === singleLinkId) {
+				item.linkUrl.forEach(function (singleLinkUrl) {
+					$("#my-button").click(function () {
 						window.open(singleLinkUrl);
+						console.log(singleLinkUrl)
 					});
-				};
-			});
-
+				});
+			};
 		});
 
 	});
+
+	
 });
 
 
